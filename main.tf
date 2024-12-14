abd0ba9b4b78895b9d8c2183a37db237b3ae7e45
Rajasekhar Betha <betha.rajasekhar@outlook..com>
2024-12-14 13:52:36 +0530

    Added the values files separately

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "EC2" {
   source = "./modules/ec2_instance"
-  ami = "ami-0cd59ecaf368e5ccf"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.instance_type
 }
--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -2,5 +2,3 @@ resource "aws_instance" "example" {
   ami = var.ami
   instance_type = var.instance_type
 }
-
-
